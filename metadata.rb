53bd79f1f3271843c9b43dfbbd1c4a6b64fc2f66
Christopher Deutsch <chrisd@invoca.com>
2014-04-30 14:45:56 -0700

    bump version to 0.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "paul.thomas@livingsocial.com"
 license          "Apache 2.0"
 description      "Installs/Configures reboot"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 depends          "chef_handler"
