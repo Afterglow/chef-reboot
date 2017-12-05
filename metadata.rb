907a2073b37dc15b6117e31c2b4977e663eb86d9
Michael Mraz <michael.mraz@cmegroup.com>
2017-12-05 11:12:14 -0600

    Foodcritic metadata info

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,7 @@ maintainer_email 'paul@paulthomas.eu'
 license          'Apache 2.0'
 description      'Installs/Configures reboot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/Afterglow/chef-reboot.git'
+issues_url       'https://github.com/Afterglow/chef-reboot/issues'
 version          '1.0.1'
 depends          'chef_handler'
